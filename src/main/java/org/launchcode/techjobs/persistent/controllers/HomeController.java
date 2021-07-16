bb46abc556c614708b32bf7995415cd6afdda680
Brett Wiegert <blwhx9@mst.edu>
2021-07-16 00:37:04 -0500

    Assignment 4 'Completed'

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,19 +1,37 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    JobRepository jobRepository;
+
+    @Autowired
+    EmployerRepository employerRepository;
+
+    @Autowired
+    SkillRepository skillRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -26,7 +44,7 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
-        model.addAttribute(new Job());
+        model.addAttribute("jobs", employerRepository.findAll());
         return "add";
     }
 
@@ -38,6 +56,9 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
+        List<Skill> jobSkills = (List<Skill>)skillRepository.findAllById(skills);
+        newJob.setSkills(jobSkills);
+        jobRepository.save(newJob);
 
         return "redirect:";
     }
